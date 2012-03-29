0587dbb1b10017ba0e6a546292d34a4f025d6f3a
Charles Collum <charles.collum@gmail.com>
2012-03-28 23:13:57 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Aavco::Application.config.secret_token = 'aeeddab2fb97d970ce19a072dc9513bf088faa552755eba2d33b7bd7d078f8213fd4202d4a7961240dbc806c5a1fd9aed87e35f35005912fc5f8b8f943580f8e'
